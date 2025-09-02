b3bad8c6b117c6a2069556e0e9784c685f249026
marjorie40 <marjoriemazeau2@gmail.com>
2025-09-02 14:41:35 +0200

    First commit

---
+++ exception/MyException.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class MyException extends Exception {
+
+    public MyException() {
+        super();
+    }
+    public MyException(String message) {
+
+        super(message);
+    }
+
+}
